bb1c25b9b65c171627f00e828571358abdebf373
Nay Myo Htet <naymyo.htet@wavemoney.com.mm>
2025-06-12 20:49:38 +0630

    Add simple Java Observer and Observable event model

---
+++ src/main/java/dev/naymyohtet/headfirst/designpatterns/observer/simpleobservable/Example.java
@@ -0,0 +1,14 @@
+package dev.naymyohtet.headfirst.designpatterns.observer.simpleobservable;
+
+
+
+public class Example {
+    public static void main(String[] args) {
+       SimpleSubject simpleSubject = new SimpleSubject();
+
+        SimpleObserver simpleObserver = new SimpleObserver(simpleSubject);
+
+        simpleSubject.setValue(10);
+
+    }
+}
---
+++ src/main/java/dev/naymyohtet/headfirst/designpatterns/observer/simpleobservable/SimpleSubject.java
@@ -0,0 +1,22 @@
+package dev.naymyohtet.headfirst.designpatterns.observer.simpleobservable;
+
+import java.util.Observable;
+
+public class SimpleSubject extends Observable {
+    private int value = 0;
+
+    public SimpleSubject() {
+
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+        setChanged();
+        notifyObservers(value);
+    }
+
+    public int getValue() {
+        return this.value;
+    }
+
+}
